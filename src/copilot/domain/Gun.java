ed9ee1bcc223da8b2527b9997bea378970d46022
Joris <Joris@Laptop-Joris>
2015-10-12 10:28:25 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/copilot/domain/Gun.java
+++ src/copilot/domain/Gun.java
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package copilot.airplane;
+package copilot.domain;
 
-import copilot.controller.User;
 import java.awt.Point;
 
 /**
